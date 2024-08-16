655e1da4e6cb2c673a7aa3b4887ca3d0d02b15da
4510514 <dejuha0235@gmail.com>
2024-08-16 20:38:25 +0300

    commit

--- _in_python.py
+++ _in_python.py
@@ -1,1 +1,17 @@
-print("This is python.")
+#checking import
+
+'''import  os
+file = os.path.exists('my_file.txt')
+
+if int(file) == 0:
+    print("file doesn't exist")'''
+fruits=['apples', 'oranges', 'banana' ]
+'''print(fruits)
+print(len(fruits))
+fruits.append('strawberries')
+print(fruits)
+fruits1 = fruits[1]
+print(fruits1)
+fruits1 = fruits[:]
+print(fruits1)'''
+
--- env1/check_push.py
+++ env1/check_push.py
@@ -1,1 +1,3 @@
-print('testing pushing')
+
+
+
